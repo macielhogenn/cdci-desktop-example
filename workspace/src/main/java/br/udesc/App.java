ac82d3500f98b25b59ff72f25c8b423d52568c94
Maciel Hogenn <maciel.hog@gmail.com>
2017-11-24 09:27:26 -0200

    Add program

---
+++ workspace/src/main/java/br/udesc/App.java
@@ -0,0 +1,10 @@
+package br.udesc;
+
+import javax.swing.*;
+
+public class App {
+
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Hello, World");
+    }
+}
